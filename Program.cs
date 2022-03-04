86c118f45fa074ddeb6ce21af12769daf537b643
gudivadasusmitha <Gudivada.Susmitha@ibm.com>
2022-03-04 21:27:16 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destructors
+{
+    /*
+    internal class complex
+    {
+        int real, img;
+        public complex()
+        {
+            real = 0;
+            img = 0;
+        }
+        public void SetValue(int r, int i)
+        {
+            real = r;
+            img = i;
+        }
+        public void DisplayValue()
+        {
+            Console.WriteLine("real = " + real);
+            Console.WriteLine("imaginary= " + img);
+        }
+        //Complex()
+        //{
+        //    Console.WriteLine("destructor was called");
+
+        //}
+    }
+    class Complex
+    {
+
+
+        static void Main(string[] args)
+        {
+            complex c = new complex();
+            c.SetValue(2, 3);
+            c.DisplayValue();
+
+        }
+    
+        
+    }*/
+    /*
+    class Generic<T>
+    {
+        public Generic(T msg)
+        {
+            Console.WriteLine(msg);
+        }
+        public void Show<T>(T msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generic<string> gen = new Generic<string>("This is generic class");
+            Generic<int> genI = new Generic<int>(101);
+            Generic<char> getCh = new Generic<char>('I');
+            gen.Show("This is generic method");
+            gen.Show(101);
+            gen.Show('I');
+        }
+    }
+}
+*/
